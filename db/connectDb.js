45075681a42eecd58a9844c51d55257dac6525c5
Sumit Dhiman <132739528+sumitdhiman77@users.noreply.github.com>
2025-11-18 13:01:55 +0530

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,19 +1,21 @@
 "use server";
 import mongoose from "mongoose";
+
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+
   try {
-    const conn = await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 20000,
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "getmeachai",
     });
   } catch (error) {
-    throw error; // Rethrow to be caught later
+    console.error("DB Connection Error:", error);
+    throw error;
   }
 };
 
 export const getConnectionState = async () => {
-  if (!mongoose.connection) {
+  if (!mongoose.connection.readyState) {
     await connectDB();
   }
   return mongoose.connection.readyState;
